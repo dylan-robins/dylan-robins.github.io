f8c7c76eff54435df1db019739d49543ba434ef5
dylan-robins <dylan.robins@gmail.com>
2019-10-24 19:59:34 +0200

    Added URL matching to matching logig and Reddit to links

--- dome.js
+++ dome.js
@@ -34,30 +34,36 @@
 // div.innerHTML : {a.innerHTML : a.href}
 var sites = {
 			"Social": {
-                "Discord"     : "https://discordapp.com",
-                "Messenger"   : "https://messenger.com",
+                "Reddit"            : "https://reddit.com",
+                "Discord"           : "https://discordapp.com",
+                "Messenger"         : "https://messenger.com",
 				"Github"			: "https://github.com/dylan-robins",
 				"YouTube"			: "https://www.youtube.com/",
 			},
 			"Work": {
 				"Gmail"				: "https://mail.google.com/mail/u/0/",
-                "UGA mail"    : "https://webmail.etu.univ-grenoble-alpes.fr/mail",
-                "Drive"       : "https://drive.google.com/drive/",
-                "Calendar"       : "https://calendar.google.com/calendar/r"
+                "UGA mail"          : "https://webmail.etu.univ-grenoble-alpes.fr/mail",
+                "Drive"             : "https://drive.google.com/drive/",
+                "Calendar"          : "https://calendar.google.com/calendar/r"
 			},
 			"My stuff": {
-                "Music"          : "https://play.google.com/music/listen#/home",
-                "localhost:5000" : "http://localhost:5000",
-                "localhost:8000" : "http://localhost:8000"
+                "Music"             : "https://play.google.com/music/listen#/home",
+                "localhost:5000"    : "http://localhost:5000",
+                "localhost:8000"    : "http://localhost:8000"
 			}
 		};
 
-var search = "https://duckduckgo.com/";		// The search engine
+var search = "https://google.fr/";		    // The search engine
 var query  = "q";							// The query variable name for the search engine
 
 var pivotmatch = 0;
 var totallinks = 0;
 var prevregexp = "";
+
+function validURL(myURL) {
+    var pattern = new RegExp('^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$', "g");
+    return pattern.test(myURL);
+}
 
 // ---------- BUILD PAGE ----------
 function matchLinks(regex = prevregexp) {
@@ -86,10 +92,12 @@ function matchLinks(regex = prevregexp) {
 				link.href = sites[sn][ln];
 				link.innerHTML = ln;
 				if (!pivotbuffer++ && regex != "") {
+                    // if we found a match, navigate to it
 					link.className = "selected";
 					document.getElementById("action").action = sites[sn][ln];
 					document.getElementById("action").children[0].removeAttribute("name");
-				}
+                    console.log(document.getElementById("action"));
+                }
 				inner.appendChild(link);
 				matches = true;
 				gmatches = true;
@@ -99,9 +107,17 @@ function matchLinks(regex = prevregexp) {
 		section.appendChild(inner);
 		matches ? p.appendChild(section) : false;
 	}
-	if (!gmatches || regex == "") {
+    if (validURL(regex)) {
+        // go directly to provided url instead of googling it
+        console.log(regex);
+		document.getElementById("action").action = 'https://' + regex;
+        document.getElementById("action").children[0].removeAttribute("name");
+        console.log(document.getElementById("action"));
+    } else if (!gmatches || regex == "") {
+        // otherwise google it
 		document.getElementById("action").action = search;
 		document.getElementById("action").children[0].name = query;
+        console.log(document.getElementById("action"));
 	}
 	document.getElementById("main").style.height = document.getElementById("main").children[0].offsetHeight+"px";
 }
